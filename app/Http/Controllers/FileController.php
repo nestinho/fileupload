ff6d7f3f6b88fd7be04764718422a1c1cc8cb4bd
Nestinho <nestory2008@gmail.com>
2022-03-29 10:16:17 +0300

    Working with files
    uploading files in laravel.

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\File;
+
+class FileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $files = File::orderBy('created_at','DESC')->paginate(30);
+        return view('file.index',['files' => $files]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('file.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //You may validate your input request this way
+        // $this->validate($request, [
+        //     'file' => 'required | file | max : 20000'
+        // ]);
+
+        // Or this way
+        $request->validate([
+            'file' => 'required | file | max : 20000'
+        ]);
+
+        $upload = $request->file('file');
+        $path = $upload->store('public/storage');
+        $file = File::create([
+            'title' => $upload->getClientOriginalName(),
+            'path' => $path,
+        ]);
+        $file->save();
+
+        return redirect('/file')->with('success','File was successfully uploaded!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
